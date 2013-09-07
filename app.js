f483b1adf7455b5510f74607d6ad8689a629b68b
Ishan Ahuja <ishanahuja@gmail.com>
2013-09-07 22:40:04 +0530

    fix app.js

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-var snake = function(){
-	this.position = {
-	};
-};
+var express = require('express'),
+    faye = require('faye');
+    snake = require('./snake.js');
 
-exports.snake = snake;
+var app = express();
+var bayeux = new faye.NodeAdapter({
+    mount: '/faye',
+    timeout: 45
+});
+
+app.configure(function(){
+	app.set('title', 'Social Snake');
+	app.use(express.bodyParser());
+	app.use(express.static(__dirname + '/public'));
+	app.engine('jade', require('jade').__express);
+});
+
+app.post('/message', function(req, res) {
+    bayeux.getClient().publish('/ssnakeMoves', { text: req.body.message });
+    console.log('broadcast message:' + req.body.message);
+    res.send(200);
+});
+
+bayeux.attach(app);
+app.listen(1337);
